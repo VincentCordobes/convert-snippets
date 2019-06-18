f21e7aff3cd569fc962e80f03e54f4ed07178916
tofulm <tofulm@gmail.com>
2019-06-18 22:01:11 +0200

    Import json to Ultisnips if body is an array (multiline), we split it. it avoids {' ,' $0 ', '}

--- src/generate.js
+++ src/generate.js
@@ -1,39 +1,50 @@
 const fs = require('fs')
-const { parse } = require('./parse')
+const {parse} = require('./parse')
 
 function generate(input, output) {
-  const source = fs.readFileSync(input)
+	const source = fs.readFileSync(input)
 
-  const isUltisnips = /.snippets$/.test(input)
-  if (isUltisnips) {
-    const json = ultisnipsToJSON(source)
-    fs.writeFileSync(output, json)
-  } else {
-    const ultisnips = jsonToUltisnips(source)
-    fs.writeFileSync(output, ultisnips)
-  }
+	const isUltisnips = /.snippets$/.test(input)
+	if (isUltisnips) {
+		const json = ultisnipsToJSON(source)
+		fs.writeFileSync(output, json)
+	} else {
+		const ultisnips = jsonToUltisnips(source)
+		fs.writeFileSync(output, ultisnips)
+	}
 }
 
 function ultisnipsToJSON(ultisnips) {
-  const snippets = parse(ultisnips)
-  return JSON.stringify(snippets, null, 2)
+	const snippets = parse(ultisnips)
+	return JSON.stringify(snippets, null, 2)
 }
 
 function jsonToUltisnips(json) {
-  const snippets = Object.values(JSON.parse(json))
-  return snippets.map(toUltisnips).join('\n')
+	const snippets = Object.values(JSON.parse(json, (key, value) => {
+		if (key === 'body') {
+			let retour = ''
+			if (Array.isArray(value)) {
+				value.forEach((e) => {
+					retour += e + '\n'
+				})
+				return retour
+			}
+		}
+		return value;
+	}))
+	return snippets.map(toUltisnips).join('\n')
 }
 
 function toUltisnips(snippet) {
-  // prettier-ignore
-  return `
+	// prettier-ignore
+	return `
 snippet ${snippet.prefix} ${snippet.description ? `"${snippet.description}"` : ''}
 ${snippet.body}
 endsnippet`
 }
 
 module.exports = {
-  generate,
-  ultisnipsToJSON,
-  jsonToUltisnips,
+	generate,
+	ultisnipsToJSON,
+	jsonToUltisnips,
 }
